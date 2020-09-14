81ab72bbb5fff97293df56f1a1d52d09051ac35e
Yaroslav Klymenko <example@gmail.com>
2020-09-14 16:58:13 +0300

    add second practice

--- Js/first practice.js
+++ Js/first practice.js
@@ -1,3 +1,4 @@
+'use stcrict';
 const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
 const personalMovieDB = {
@@ -8,8 +9,26 @@ const personalMovieDB = {
     privat: false,
 };
 
-const oneOfTheLastFilms = prompt('Один из последлних фильмов?', '');
-const rating = prompt('на сколько его оцените ?', '');
+for (let i = 0; i < 2; i++) {
+    const oneOfTheLastFilms = prompt('Один из последлних фильмов?', ''),
+          rating = prompt('на сколько его оцените ?', '');
 
-personalMovieDB.movies[oneOfTheLastFilms] = rating; 
+
+    if (oneOfTheLastFilms != null && rating != null && oneOfTheLastFilms != '' && rating != '' && oneOfTheLastFilms.length < 50 && rating.length < 50) {
+        personalMovieDB.movies[oneOfTheLastFilms] = rating;
+        console.log('done');
+    } else {
+        alert('сделай все правильно');
+        i--;
+    }
+}
+
+if (personalMovieDB.count < 10) {
+    alert('Вы просмотрели очень мало фильмов!');
+} else if (personalMovieDB.count > 10 && personalMovieDB.count < 30) {
+    alert('Вы бывалый зритель');
+} else {
+    alert ('Что-то пошло не так!');
+}
+
 console.log(personalMovieDB);
