6f84389d9d2b9768ea4f5295e61e39e3652d341c
Yaroslav Klymenko <example@gmail.com>
2020-09-18 17:08:26 +0300

    add practice with objects

--- Js/first practice.js
+++ Js/first practice.js
@@ -1,70 +1,70 @@
 'use stcrict';
 
-let numberOfFilms;
-
-function start() {
-    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-
-    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
-        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-    }
-}
-
-start();
-
 const personalMovieDB = {
-    count: numberOfFilms,
+    count: 0,
     movies: {},
     actors: {},
     genres: [],
     privat: false,
-};
+    start: function () {
+        personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-function rememberMyFlims() {
-    for (let i = 0; i < 2; i++) {
-        const oneOfTheLastFilms = prompt('Один из последлних фильмов?', ''),
-            rating = prompt('на сколько его оцените ?', '');
+        while (personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
+            personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
+        }
+    },
+    rememberMyFlims: function () {
+        for (let i = 0; i < 2; i++) {
+            const oneOfTheLastFilms = prompt('Один из последлних фильмов?', ''),
+                rating = prompt('на сколько его оцените ?', '');
 
 
-        if (oneOfTheLastFilms != null && rating != null && oneOfTheLastFilms != '' && rating != '' && oneOfTheLastFilms.length < 50 && rating.length < 50) {
-            personalMovieDB.movies[oneOfTheLastFilms] = rating;
-            console.log('done');
+            if (oneOfTheLastFilms != null && rating != null && oneOfTheLastFilms != '' && rating != '' && oneOfTheLastFilms.length < 50 && rating.length < 50) {
+                personalMovieDB.movies[oneOfTheLastFilms] = rating;
+                console.log('done');
+            } else {
+                alert('сделай все правильно');
+                i--;
+            }
+        }
+    },
+    showMyDB: function () {
+        if (personalMovieDB.privat === false) {
+            console.log(personalMovieDB);
+        }
+    },
+    detectPersonalLevel: function () {
+        if (personalMovieDB.count < 10) {
+            alert('Вы просмотрели очень мало фильмов!');
+        } else if (personalMovieDB.count > 10 && personalMovieDB.count < 30) {
+            alert('Вы бывалый зритель');
+        } else if (personalMovieDB.count >= 30) {
+            alert('Вы настоящий киноман!');
         } else {
-            alert('сделай все правильно');
-            i--;
+            alert('Что-то пошло не так!');
+        }
+    },
+    writeYourGenres: function () {
+        for (let i = 1; i <= 3; i++) {
+            let genre = prompt(`Ваш любимый жанр подномером ${i}`, '');
+
+            if(genre === '' || genre == null) {
+                console.log('Wrong Data or Empty'); 
+                    i--;
+                
+            } else {
+                personalMovieDB.genres.push(genre);
+            }
+        }
+        personalMovieDB.genres.forEach((item, i) => {
+            console.log(`Любимый жанр ${i + 1} - это ${item}`);
+        });
+    },
+    toggleVisibleMyDB: function () {
+        if (personalMovieDB.privat) {
+            personalMovieDB.privat = false;
+        } else {
+            personalMovieDB.privat = true;
         }
     }
-}
-
-rememberMyFlims();
-
-
-function detectPersonalLevel() {
-    if (personalMovieDB.count < 10) {
-        alert('Вы просмотрели очень мало фильмов!');
-    } else if (personalMovieDB.count > 10 && personalMovieDB.count < 30) {
-        alert('Вы бывалый зритель');
-    } else if (personalMovieDB.count >= 30) {
-        alert('Вы настоящий киноман!');
-    } else {
-        alert('Что-то пошло не так!');
-    }
-}
-
-detectPersonalLevel();
-
-function showMyDB() {
-    if (personalMovieDB.privat === false) {
-        console.log(personalMovieDB);
-    }
-}
-
-showMyDB();
-
-function writeYourGenres() {
-    for (let i = 1; i <= 3; i++) {
-        personalMovieDB.genres.push(prompt(`Ваш любимый жанр подномером ${i}`, ''));
-    }
-}
-
-writeYourGenres();
+};
--- Js/script.js
+++ Js/script.js
@@ -1,1 +1,75 @@
- 
+//  const options = {
+//     name: 'test',
+//     width: 1024,
+//     heigth: 1024,
+//     colors: {
+//         border: 'black',
+//         bg: 'red'
+//     },
+//     makeTest: function() {
+//         console.log('Test');
+//     },
+//  };
+
+//  let descriptor = Object.getOwnPropertyDescriptor(options, 'name');
+//  console.log(descriptor);
+
+//  const {border,bg} = options.colors;
+
+//  console.log(border);
+//  console.log(bg);
+//  console.log(String.prototype);
+
+//  options.makeTest();
+//  for (let key in options) {
+//      if (typeof(options[key]) === 'object') {
+//          for (let i in options[key]) {
+//             console.log(`Свовойство ${i} в Объекте ${options[key]} имеет значения ${options[key][i]}`)
+//          }
+//      } else {
+//         console.log(`Свовойство ${key} имеет значения ${options[key]}`);
+//      }
+
+//  }
+
+//  options.length = -1;
+
+//  for (let key in options) {
+//      options.length += 1;
+//  }
+
+//  console.log(options.length);
+//  console.log(Object.entries(options));
+
+// const arr = [1, 2, 3, 4, 5];
+
+// console.log(arr.sort((a, b) => b - a));
+
+// const mainObj = {
+//     a: 1,
+//     b: 2,
+//     c: {
+//         d: 3,
+//         e: 4,
+//     },
+// };
+
+// const inObj = {
+//     f: 5,
+//     g: 6,
+// };
+
+// console.log(Object.assign(mainObj, inObj));
+
+const Car = {
+    go:function() {
+    },
+    have4wheels: true,
+};
+
+const BMW = Object.create(Car);
+
+BMW.premium = true;
+
+console.log(BMW);
+console.log(Car);
